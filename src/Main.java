b6a3ab103f3dd60c4d3ed7390abc12f204bc6d81
cristiansystem <cristian.1994.lf@gmail.com>
2022-06-03 23:33:43 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setName("Jhon Doe");
+        person.setAge(25);
+        person.setPhone(695857414);
+        person.showPersonalInfo();
+    }
+}
